744e2ba08b321bbeaadcf6a1d9763d3c3ed5f39d
Benjamin Leduc <giby@users.noreply.github.com>
2025-02-09 17:47:12 +0100

    Update structure.h

--- structure.h
+++ structure.h
@@ -1,37 +1,36 @@
-
 /*Mechanics of the program, should not need to be changed. */
-#define UNDERFLO   1e-100 /*DBL_MIN*1.1*/	/*Get worried about numbers that are smaller than this */
-#define STOREFREQ  1		/*frequency of recording the likelihood */
-#define UNASSIGNED -9		/*missing Data are placed in this population. Should be <0 */
-#define STRLEN     200		/* max length of string used to store data file name */
+#define UNDERFLO   1e-100 /*DBL_MIN*1.1*/  /*Get worried about numbers that are smaller than this */
+#define STOREFREQ  1        /*frequency of recording the likelihood */
+#define UNASSIGNED -9       /*missing Data are placed in this population. Should be <0 */
+#define STRLEN     200      /* max length of string used to store data file name */
 #define GENELEN 15              /* max length of gene name */
-#define LABELLEN   12		/* max length of string used to store individ label */
-#define VERBOSE    0		/* (B) Print run-time details to screen */
-#define ALLELLEN   10		/* integer data in data file are read in as strings 
-				   of this length */
-#define MAXINDS    3		/* print 2*MAXINDS-1 individuals when printing data to screen */
-#define BANNERFREQ 10		/*freq of printing banner for update stuff */
+#define LABELLEN   12       /* max length of string used to store individ label */
+#define VERBOSE    0        /* (B) Print run-time details to screen */
+#define ALLELLEN   10       /* integer data in data file are read in as strings 
+                               of this length */
+#define MAXINDS    3        /* print 2*MAXINDS-1 individuals when printing data to screen */
+#define BANNERFREQ 10       /*freq of printing banner for update stuff */
 #define LAMBDAMAX  10           /*max value of lambda*/
 #define LAMBDAPROPSD 0.3          /*SD for proposal for lambda*/
 
 /*return appropriate positions in each array... */
-#define GenPos(ind,line,loc) ((ind)*(LINES)*(NUMLOCI)+(line)*(NUMLOCI)+(loc))	/*Geno */
-#define TransPos(loc,pos) ((MAXALLELES)*(loc)+(pos))	/*Translation */
-#define ZPos(ind,line,loc) ((ind)*(LINES)*(NUMLOCI)+(line)*(NUMLOCI)+(loc))	/* Z */
+#define GenPos(ind,line,loc) ((ind)*(LINES)*(NUMLOCI)+(line)*(NUMLOCI)+(loc))  /*Geno */
+#define TransPos(loc,pos) ((MAXALLELES)*(loc)+(pos))  /*Translation */
+#define ZPos(ind,line,loc) ((ind)*(LINES)*(NUMLOCI)+(line)*(NUMLOCI)+(loc))  /* Z */
 #define SiteBySiteSumPos(ind,line,loc,pop) ((ind)*(LINES)*(NUMLOCI)*(MAXPOPS)+(line)*(NUMLOCI)*(MAXPOPS)+(loc)*(MAXPOPS)+(pop))
-	/* P */
+  /* P */
 #define DiploidSiteBySiteSumPos(ind,pop2,loc,pop) ((ind)*(MAXPOPS)*(NUMLOCI)*(MAXPOPS)+(pop2)*(NUMLOCI)*(MAXPOPS)+(loc)*(MAXPOPS)+(pop))
-	
+  
 #define PPos(loc,pop,allele) ((loc)*(MAXPOPS)*(MAXALLELES)+(pop)*(MAXALLELES)+(allele))
-#define QPos(ind,pop) ((ind)*(MAXPOPS)+(pop))	/* Q */
+#define QPos(ind,pop) ((ind)*(MAXPOPS)+(pop))  /* Q */
 #define RTransitProbPos(loc,line,pop) ((loc)*MAXPOPS*LINES+(line)*MAXPOPS+(pop))
 #define DiploidRTransitProbPos(loc,pop,pop2) ((loc)*MAXPOPS*MAXPOPS+(pop)*MAXPOPS+(pop2))
-#define EpsPos(loc,allele) ((loc)*(MAXALLELES)+(allele))	/* Epsilon */
-#define NumAFromPopPos(pop,allele) ((pop)*(MAXALLELES)+(allele))	/* NumAFromPop */
-	/*AncestDist */
+#define EpsPos(loc,allele) ((loc)*(MAXALLELES)+(allele))  /* Epsilon */
+#define NumAFromPopPos(pop,allele) ((pop)*(MAXALLELES)+(allele))  /* NumAFromPop */
+  /*AncestDist */
 #define AncestDistPos(ind,pop,box) ((ind)*(MAXPOPS)*(NUMBOXES)+(pop)*(NUMBOXES)+(box))
-#define PostProbsPos(pop,gen) ((pop)*(GENSBACK+1)+(gen))	/*PostProbs-Q update with pop */
-	/*UsePopProbs */
+#define PostProbsPos(pop,gen) ((pop)*(GENSBACK+1)+(gen))  /*PostProbs-Q update with pop */
+  /*UsePopProbs */
 #define UsePPrPos(ind,pop,gen) ((ind)*(MAXPOPS)*(GENSBACK+1)+(pop)*(GENSBACK+1)+(gen))
 #define PhasePos(ind,loc)   ((ind)*(NUMLOCI)+(loc))
 #define SquarePos(pop,pop2) ((pop)*(MAXPOPS)+(pop2))
@@ -50,7 +49,7 @@
 /*GLOBAL VARIABLES WHICH ARE SET IN THE mainparams AND extraparams FILES;
    SEE EXPLANATIONS THERE */
 
-struct IND {				/*Non-genotype info for each individual */
+struct IND {   /*Non-genotype info for each individual */
   char Label[LABELLEN];
   int Population;
   int PopFlag;
@@ -78,7 +77,7 @@ int MAXPOPS, BURNIN, NUMREPS;
 int USEPOPINFO, INFERALPHA, INFERLAMBDA,POPSPECIFICLAMBDA, PFROMPOPFLAGONLY;
 int POPALPHAS, COMPUTEPROB, NOADMIX, ADMBURNIN;
 int MAPDISTANCES, MARKERNAMES, LINKAGE, PHASED,PHASEINFO,MARKOVPHASE;
-		   
+       
 /*Output Options */
 int UPDATEFREQ, PRINTLIKES, INTERMEDSAVE, PRINTKLD, PRINTNET, PRINTLAMBDA;
 int ECHODATA, ANCESTDIST, NUMBOXES, GENSBACK;
@@ -116,18 +115,15 @@ int SEED;
 int NOQS, POSTERIOR,PICTUREFILE,INDIVIDUALR;
 
 
-
-
 /*Needed for program (not input) */
-int MAXALLELES;			/*maximum number of alleles at any locus */
-int ORIGMISSING;		/*value of missing alleles may be changed when the
-				   data are translated; this is the original value */
+int MAXALLELES;     /*maximum number of alleles at any locus */
+int ORIGMISSING;    /*value of missing alleles may be changed when the
+                      data are translated; this is the original value */
 int ORIGMISSINGPHENO;           /*ditto for phenotypes in STRAT*/
-int NOALPHA;			/*Alpha not used because prior pop model is used instead */
+int NOALPHA;        /*Alpha not used because prior pop model is used instead */
 
 /*function declarations------ */
-void Kill ();			/*abort program */
+void Kill (void);     /*abort program */
 
 void Welcome (FILE * file);
 int FindMaxAlleles (int *Geno, int *Recessive);
-
